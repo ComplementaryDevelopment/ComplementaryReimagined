7180164ac61a634ce127899944499f382f4c5303
Emin <emingyildiz@gmail.com>
2023-09-10 11:46:53 +0300

    r5.0.1

--- shaders/program/composite.glsl
+++ shaders/program/composite.glsl
@@ -86,6 +86,8 @@ uniform sampler2D depthtex1;
 //Common Variables//
 float SdotU = dot(sunVec, upVec);
 float sunFactor = SdotU < 0.0 ? clamp(SdotU + 0.375, 0.0, 0.75) / 0.75 : clamp(SdotU + 0.03125, 0.0, 0.0625) / 0.0625;
+float sunVisibility = clamp(SdotU + 0.0625, 0.0, 0.125) / 0.125;
+float sunVisibility2 = sunVisibility * sunVisibility;
 
 vec2 view = vec2(viewWidth, viewHeight);
 
@@ -96,8 +98,6 @@ vec2 view = vec2(viewWidth, viewHeight);
 #endif
 
 #ifdef LIGHTSHAFTS_ACTIVE
-	float sunVisibility = clamp(SdotU + 0.0625, 0.0, 0.125) / 0.125;
-	float sunVisibility2 = sunVisibility * sunVisibility;
 	float shadowTimeVar1 = abs(sunVisibility - 0.5) * 2.0;
 	float shadowTimeVar2 = shadowTimeVar1 * shadowTimeVar1;
 	float shadowTime = shadowTimeVar2 * shadowTimeVar2;
