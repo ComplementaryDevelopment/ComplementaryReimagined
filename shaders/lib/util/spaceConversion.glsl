1158452ec7b4839c6b00c19eb446e5f2091c3b6a
Slaynash <hugoflores@hotmail.fr>
2024-12-05 16:53:32 +0100

    [DH] Beacon beams depth-test

--- shaders/lib/util/spaceConversion.glsl
+++ shaders/lib/util/spaceConversion.glsl
@@ -26,3 +26,17 @@ vec3 ShadowClipToShadowView(vec3 pos) {
 vec3 ShadowViewToPlayer(vec3 pos) {
     return mat3(shadowModelViewInverse) * pos;
 }
+
+// Distant Horizon
+#ifdef DISTANT_HORIZONS
+
+vec3 ScreenToViewDH(vec3 pos) {
+    vec4 iProjDiag = vec4(dhProjectionInverse[0].x,
+                          dhProjectionInverse[1].y,
+                          dhProjectionInverse[2].zw);
+    vec3 p3 = pos * 2.0 - 1.0;
+    vec4 viewPos = iProjDiag * p3.xyzz + dhProjectionInverse[3];
+    return viewPos.xyz / viewPos.w;
+}
+
+#endif
